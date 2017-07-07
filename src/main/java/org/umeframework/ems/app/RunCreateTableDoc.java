b91299b8fb4998a6584bacfcde91d96c149f90e2
mmayye <mmayye@10.0.0.101>
2017-07-07 18:47:56 +0800

    1

---
+++ src/main/java/org/umeframework/ems/app/RunCreateTableDoc.java
@@ -0,0 +1,27 @@
+package org.umeframework.ems.app;
+
+import org.umeframework.dora.tool.gen.sql.Excel2SqlGenerator;
+
+/**
+ * 1.根据Excel输入的Table列表信息及数据库连接信息，查询数据字典获取表定义的结构。<br> 
+ * 2.根据上述结构生成数据库表设计文档。<br>
+ * 
+ * @author Yue MA
+ */
+public class RunCreateTableDoc {
+
+	/**
+	 * 主函数.<br>
+	 * 允许方法：Eclipse中右键点击该类，选择[Run As] - [Java Application]选项执行.<br>
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			new Excel2SqlGenerator().createTableDoc("input/create-table-doc");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/org/umeframework/ems/app/RunServiceGenerator.java
@@ -0,0 +1,24 @@
+package org.umeframework.ems.app;
+
+import org.umeframework.dora.tool.gen.ServiceGenerator;
+
+/**
+ * ServiceGenerator
+ *
+ * @author MA YUE
+ */
+public class RunServiceGenerator {
+
+	/**
+	 * 主函数.<br>
+	 * 允许方法：Eclipse中右键点击该类，选择[Run As] - [Java Application]选项执行.<br>
+	 */
+	public static void main(String[] args) {
+		try {
+			String inputPath = "input/design-service";
+			new ServiceGenerator().execute(inputPath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
